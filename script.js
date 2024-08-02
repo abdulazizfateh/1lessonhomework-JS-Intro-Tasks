46cc6858a4691da8b62e7763b70173fe55f226bc
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-08-02 09:51:44 +0500

    JS Intro Tasks

---
+++ script.js
@@ -0,0 +1,53 @@
+// TASK 1 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// let num = 123;
+
+// let a = num % 100; // a = 23;
+// let first = (num - a) / 100; // first = 1;
+// let last = a % 10; // last = 3;
+// let middle = (a - last) / 10; // middle 2;
+
+// console.log(last * 100 + first * 10 + middle);
+
+
+
+// TASK 2 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// let num = 123;
+
+// let a = num % 100; // a = 23;
+// let first = (num - a) / 100; // first = 1;
+// let last = a % 10; // last = 3;
+// let middle = (a - last) / 10; // middle = 2;
+
+// console.log(middle * 100 + first * 10 + last);
+
+
+
+// TASK 3 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// let num = 123;
+
+// let a = num % 100; // a = 23;
+// let first = (num - a) / 100; // first = 1;
+// let last = a % 10; // last = 3;
+// let middle = (a - last) / 10; // middle = 2;
+
+// console.log(first * 100 + last * 10 + middle);
+
+
+
+// TASK 4 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// let num = 1234;
+
+// let a = num % 1000; // a = 234;
+// let b = a % 100; // b = 34;
+// let c = (a - b) / 100; 
+
+// console.log(c);
+
+
+
+// TASK 5 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// let num = 1234;
+
+// let a = num % 1000; // a = 234;
+// let first = (num - a) / 1000;
+// console.log(first);
